bf3065e4c4cc5959b015e422a514c8d027cbafd9
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-19 22:58:27 -0500

    quitando consoles

--- src/modules/auth/auth_utils.js
+++ src/modules/auth/auth_utils.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 exports.signToken = (payload, secret = process.env.JWT_SECRET_KEY, ) => {
-  console.log("🚀 ~ file: auth_utils.js ~ line 7 ~ process.env.JWT_EXPIRE_TIME", process.env.JWT_EXPIRE_TIME)
   return jwt.sign(payload, secret, {
     expiresIn: process.env.JWT_EXPIRE_TIME
   });
@@ -9,7 +8,6 @@ exports.signToken = (payload, secret = process.env.JWT_SECRET_KEY, ) => {
 
 exports.verifyToken = (token, secret = process.env.JWT_SECRET_KEY) => {
   try {
-    console.log('token', token)
     const verified = jwt.verify(token, secret);
     return {
       msj: 'Autentificado con exito',
