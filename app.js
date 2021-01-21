91898541908db02f1b22c389755b2b68b6ec8d1d
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-20 23:54:51 -0500

    agregando passport

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express    = require('express'),
       app        = express(),
       cors       = require('cors'),
       bodyParser = require('body-parser'),
-      { dbConnection } = require('./src/database/config');;
+      { dbConnection } = require('./src/database/config');
 
 require('dotenv').config();
+
+//passport stuff
+const passport = require('passport');
+const jwtStrategry  = require('./src/strategies/jwt');
+passport.use(jwtStrategry);
 
 // Hacemos la conexion a mongodb
 dbConnection();
@@ -19,8 +24,13 @@ const r_example = require('./src/modules/example/r_example');
 const r_auth = require('./src/modules/auth/r_auth');
 // Establecemos las rutas
 app
-.use('/example', r_example)
-.use('/auth', r_auth)
+  .use('/example', r_example)
+  .use('/auth', r_auth);
+
+// Ejemplo de ruta protegida
+app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+    return res.status(200).send('YAY! this is a protected Route')
+})
 
 
 app.listen(process.env.PORT, () => {
