58cab92ba79dab08f7636379be707f253e6a2d93
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-20 23:55:22 -0500

    para ver que importaciones necesito para passport

--- src/modules/example/r_example.js
+++ src/modules/example/r_example.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const c_example = require('./c_example');
 
@@ -10,5 +11,10 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get('/hola', c_example.exampleHola);
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+  return res.status(200).send({
+    example: 'este es el mensaje dentor de example'
+  })
+})
 
 module.exports = router;
