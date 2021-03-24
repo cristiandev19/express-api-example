0e0b892788fc257f34a3bfd575c93416d0a39533
cristiandev <cristiansotomayor.dev@gmail.com>
2021-03-23 22:35:31 -0500

    fix

--- src/databases/mongodb.js
+++ src/databases/mongodb.js
@@ -2,17 +2,16 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect( process.env.MONGO_CONNECT, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-
-  } catch(error) {
-    throw new Error('Algo salio mal en la coneccion a mongo')
+    await mongoose.connect(process.env.MONGO_CONNECT, {
+      useNewUrlParser    : true,
+      useUnifiedTopology : true,
+      useCreateIndex     : true,
+    });
+  } catch (error) {
+    throw new Error('Algo salio mal en la coneccion a mongo');
   }
-}
+};
 
 module.exports = {
-  dbConnection
-}
+  dbConnection,
+};
